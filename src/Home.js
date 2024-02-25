d4d581a284dbca5b2d6e167462bfdd525eaab243
ataleb251 <69352738+ataleb251@users.noreply.github.com>
2024-02-25 16:07:27 +0300

    Click Event

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,16 @@
 const Home = () => {
+    const HandlClick = () => {
+        alert('hello , ')
+    }
+    const HandlClickAgain = (name) => {
+        alert('hello , ' + name)
+    }
     return ( 
         <div className="home">
             <h2>HomePage</h2>
+            <button onClick={HandlClick}>Click Me </button>
+            <button onClick={() =>
+                HandlClickAgain('Abdalelah')}> Again </button>
         </div>
      );
 }
