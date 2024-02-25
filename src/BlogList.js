fb9bf653e48941c4ffb88e182428a2ed5451395b
ataleb251 <69352738+ataleb251@users.noreply.github.com>
2024-02-25 18:00:53 +0300

    Delete Blog

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,18 +1,16 @@
-const BlogList = ({blogs,title}) => {
-
-    return ( 
-    <div className="BlogList">
-        <h1>{title}</h1>
+const BlogList = ({ blogs, title, handleDelete }) => {
+    return (
+      <div className="blog-list">
+        <h2>{ title }</h2>
         {blogs.map(blog => (
-            
-            <div className="blog-preview" key={blog.id}>
-                <h2> {blog.title}</h2>
-                <p>Written Byt {blog.author}</p>
-            </div>
-           
+          <div className="blog-preview" key={blog.id} >
+            <h2>{ blog.title }</h2>
+            <p>Written by { blog.author }</p>
+            <button onClick={() => handleDelete(blog.id)}>delete blog</button>
+          </div>
         ))}
-        </div>
-     );
-}
- 
-export default BlogList;
+      </div>
+    );
+  }
+   
+  export default BlogList;
