9ddb4030028a466cb3e9c3020c5bbb4f11dd6f72
ataleb251 <69352738+ataleb251@users.noreply.github.com>
2024-02-25 17:47:29 +0300

    Reuseable Components and props

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import BlogList from "./BlogList";
 const Home = () => {
     const [blogs, setBlogs] = useState([
         { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
@@ -6,15 +7,9 @@ const Home = () => {
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
       ])
     return ( 
-        <div className="home">
-        {
-            blogs.map(blog => (
-                <div className="blog-preview" key={blog.id}>
-                    <h2> {blog.title}</h2>
-                    <p>Written Byt {blog.author}</p>
-                </div>
-            ))
-        }
+        <div className="home">      
+            <BlogList blogs ={blogs} title ={'All Blogs'}/>
+            <BlogList blogs ={blogs.filter((blog) => blog.author =='mario')}  title ={"Mario's Blogs"}/>
         </div>
      );
 }
