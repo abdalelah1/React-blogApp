0121c02db4fd0399b69889063773d7af1b86f27e
ataleb251 <69352738+ataleb251@users.noreply.github.com>
2024-03-11 18:36:55 +0300

    Create New Blog

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import NavBar from './NavBar';
 import Home from './Home';
 import { BrowserRouter as Router  ,Route , Routes } from 'react-router-dom';
+import CreateBlog from './Create';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
      <Routes>
 
       <Route path='/' element={<Home />} />
+      <Route path='/create' element={<CreateBlog />} />
 
 
      </Routes>
---
+++ src/Create.js
@@ -0,0 +1,9 @@
+const CreateBlog = () => {
+    return (  
+        <div className="Create">
+            <h2>Create New BLog</h2>
+        </div>
+    );
+}
+ 
+export default CreateBlog;
